1d2ecdaf6cdba9042f16bf9f0eaa3d5bfe627d75
MinhNamThan <84622781+MinhNamThan@users.noreply.github.com>
2023-08-06 13:32:22 +0700

    fix distance

--- sourcecode/src/main/java/entity/shipping/NewShippingFeeCaculator.java
+++ sourcecode/src/main/java/entity/shipping/NewShippingFeeCaculator.java
@@ -2,11 +2,11 @@ package entity.shipping;
 
 import entity.order.Order;
 
+
 public class NewShippingFeeCaculator implements ShippingFeeCaculator {
 	@Override
-	public int caculateShippingFee(int distance) {
-		int text = (int)(distance*3) + 1;
-		System.out.println("Hello " + text);
-		return text;
+	public int caculateShippingFee(Order order) {
+		int distance = order.getDeliveryInfo().distanceAdapter.calculateDistance(order.getDeliveryInfo().getAddress(), order.getDeliveryInfo().getProvince());
+		return distance;
 	}
 }
